9570d8fad52013637f3552a0ff9a7fc8cca542d9
WebMaster <umrec_web@unimex.edu.mx>
2022-08-23 17:18:03 -0500

    add regsitr prospecto

--- src/js/services/peticiones.js
+++ src/js/services/peticiones.js
@@ -1,3 +1,34 @@
 /**
  * archivo para peticiones fetch...
  */
+
+const url = '../php/procesa-datos.php';
+
+
+const registraProspecto = async( datos )=>{
+
+    if(!datos) throw 'no hay datos';
+
+    try {
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "Accept"       : "application/json",
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({datos})
+        });
+    
+        if( !response.ok ) throw 'Error al enviar los datos';
+        return await response.json();
+        
+    } catch (error) {
+
+        throw error;
+        
+    }
+
+};
+
+export{ registraProspecto };
